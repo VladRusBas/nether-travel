204c7d1b9111f71e69cc151c4ebc9e733b91e257
VladRusBas <basok.vlad.1999@gmail.com>
2019-10-06 13:37:59 +0300

    Moved "fortune cookie" functionality into module.

--- nether.js
+++ nether.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
-var app = express();
-
+var fortune = require('./lib/fortune.js')
 var handlebars = require('express-handlebars')
     .create({ defaultLayout: 'main' });
+
+var app = express();
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -12,21 +12,12 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortunes = [
-    "Defeat your fears or your fears defeat you.",
-    "Rivers need sources.",
-    "Don't fear unfamilliar.",
-    "You're gonna be nicely surprised.",
-    "Be simpler wherever you can be."
-];
-
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 //Handling 404 error
